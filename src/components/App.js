2840ad194e48d7931f26878ca1622a2546294e8c
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-20 06:44:39 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,4 @@ import FruitBasket from './FruitBasket';
 
 const App = () => <FruitBasket />;
 
-export default class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      fruitList: []
-    }
-  }
-};
+export default App;
