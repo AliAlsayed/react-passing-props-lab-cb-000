d0612ab6573b2e2e4064e0fa281b0df3adc27573
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-20 06:50:19 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-const App = () => <FruitBasket />;
+//const App = () => <FruitBasket />;
 
 class App extends React.Component {
   constructor(props) {
