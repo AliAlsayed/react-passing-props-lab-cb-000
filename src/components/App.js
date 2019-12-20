7c39c4acedf4c4652f32f4a651cb94ab31c645c9
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-20 06:43:42 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FruitBasket from './FruitBasket';
 
-//const App = () => <FruitBasket />;
+const App = () => <FruitBasket />;
 
 export default class App extends React.Component {
   constructor(){
