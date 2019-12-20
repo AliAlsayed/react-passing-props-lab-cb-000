9496c311f71973840220a5876d74c79890e80b16
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-20 06:50:13 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -4,4 +4,15 @@ import FruitBasket from './FruitBasket';
 
 const App = () => <FruitBasket />;
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      fruit: [],
+      filter: [],
+      currentFilters: null
+    };
+  }
+}
 export default App;
