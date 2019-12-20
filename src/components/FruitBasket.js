6a192ac4813997e72b684430cbad63835481fcab
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-20 07:12:11 +0000

    Automatically backed up by Learn

--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -14,20 +14,11 @@ const FruitBasket = ({fruit, filters, currentFilter, onUpdateFilter}) =>
   </div>
 
 
-  handleFilterChange = event => {
-    console.log('new filter: ', event.target.value);
-    this.setState({ selectedFilter: event.target.value });
-  }
-
-  render() {
-    return (
-      <div className="fruit-basket">
-        <Filter handleChange={this.handleFilterChange} />
-        <FilteredFruitList
-          filter={this.state.selectedFilter} />
-      </div>
-    );
-  }
+FruitBasket.defaultProps = {
+  fruit: [],
+  filters: [],
+  currentFilter: null,
+  updateFilterCallback: () => {}
 }
 
 export default FruitBasket;
